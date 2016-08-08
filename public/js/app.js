5737034f9ef26f5f4a7ed1fc71a3934e4521e446
danielhg7 <danielhg7@gmail.com>
2016-08-08 01:38:23 -0300

    Add toastr after response of success in register

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-angular.module('apreakApp', ['ngResource', 'ngRoute', 'appRoutes', 'AuthCtrl', 'MainCtrl', 'NerdCtrl', 'RegisterCtrl', 'NerdService', 'AuthService', 'RegisterService']);
+angular.module('apreakApp', ['ngResource', 'ngRoute', 'ngAnimate', 'toastr', 'appRoutes', 'AuthCtrl', 'MainCtrl', 'NerdCtrl', 'RegisterCtrl', 'NerdService', 'AuthService', 'RegisterService'])
+	
+
+	.config(function (toastrConfig) {
+		angular.extend(toastrConfig, {
+  			positionClass: 'toast-bottom-right'
+  		});
+  	});
--- public/js/services/RegisterService.js
+++ public/js/services/RegisterService.js
@@ -1,9 +1,12 @@
-angular.module('RegisterService', []).factory('Register', ['$http', function($http) {
+angular.module('RegisterService', []).factory('Register', function($http, toastr) {
 
     return {
         registerUser:function(credentials){
     		console.log(credentials)
             $http.post('/register', {data:credentials})
+            .success( function(response){
+            	toastr.success(response.message);
+            });
         }
     }
-}]);
+});
